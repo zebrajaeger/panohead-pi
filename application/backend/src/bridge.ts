b80c4c328a15838e8e8ba2557e277d2c22c02ab6
Lars Brandt <zebrajaeger@zebrajaeger.de>
2020-06-03 11:37:55 +0200

    implement focus and trigger for camera

--- application/backend/src/bridge.ts
+++ application/backend/src/bridge.ts
@@ -15,16 +15,24 @@ export interface Joystick {
     y: number;
 }
 
+export interface Camera {
+    isFocussing: boolean;
+    isTriggering: boolean;
+}
+
 export interface Status {
     actor: Actor;
     joystick: Joystick;
+    camera: Camera;
 }
 
 enum Commands {
     writeLimit = 0,
-
     writeVelocity = 20,
-    writePos = 21
+    writePos = 21,
+
+    triggerCameraFocus = 30,
+    triggerCameraTrigger = 31,
 }
 
 export class Bridge {
@@ -34,28 +42,42 @@ export class Bridge {
 
     writeLimit(axis: number, velocityMinHz: number, velocityMaxHz: number, accelerationMaxHzPerSecond: number): void {
         let buffer = Buffer.alloc(11);
-        this.writeU8(buffer, 0, Commands.writeLimit);
-        this.writeU8(buffer, 1, axis);
-        this.writeI24(buffer, 2, velocityMinHz);
-        this.writeI24(buffer, 5, velocityMaxHz);
-        this.writeI24(buffer, 8, accelerationMaxHzPerSecond);
+        this.write8(buffer, 0, Commands.writeLimit);
+        this.write8(buffer, 1, axis);
+        this.write24(buffer, 2, velocityMinHz);
+        this.write24(buffer, 5, velocityMaxHz);
+        this.write24(buffer, 8, accelerationMaxHzPerSecond);
         this.i2c.i2cWriteSync(this.i2cAddress, buffer.length, buffer);
     }
 
     writePos(axis: number, pos: number): void {
         let buffer = Buffer.alloc(5);
-        this.writeU8(buffer, 0, Commands.writePos);
-        this.writeU8(buffer, 1, axis);
-        this.writeI24(buffer, 2, pos);
+        this.write8(buffer, 0, Commands.writePos);
+        this.write8(buffer, 1, axis);
+        this.write24(buffer, 2, pos);
         console.log(buffer);
         this.i2c.i2cWriteSync(this.i2cAddress, buffer.length, buffer);
     }
 
     writeVelocity(axis: number, velocity: number): void {
         let buffer = Buffer.alloc(5);
-        this.writeU8(buffer, 0, Commands.writeVelocity);
-        this.writeU8(buffer, 1, axis);
-        this.writeI24(buffer, 2, velocity);
+        this.write8(buffer, 0, Commands.writeVelocity);
+        this.write8(buffer, 1, axis);
+        this.write24(buffer, 2, velocity);
+        this.i2c.i2cWriteSync(this.i2cAddress, buffer.length, buffer);
+    }
+
+    triggerCameraFocus(durationMs: number) {
+        let buffer = Buffer.alloc(5);
+        this.write8(buffer, 0, Commands.triggerCameraFocus);
+        this.write32(buffer, 1, durationMs);
+        this.i2c.i2cWriteSync(this.i2cAddress, buffer.length, buffer);
+    }
+
+    triggerCameraTrigger(durationMs: number) {
+        let buffer = Buffer.alloc(5);
+        this.write8(buffer, 0, Commands.triggerCameraTrigger);
+        this.write32(buffer, 1, durationMs);
         this.i2c.i2cWriteSync(this.i2cAddress, buffer.length, buffer);
     }
 
@@ -65,10 +87,11 @@ export class Bridge {
                 x: {isAtPosition: true, position: 0},
                 y: {isAtPosition: true, position: 0}
             },
-            joystick: {x: 0, y: 0}
+            joystick: {x: 0, y: 0},
+            camera: {isFocussing: false, isTriggering: false}
         }
 
-        let buffer = Buffer.alloc(11);
+        let buffer = Buffer.alloc(12);
         this.i2c.i2cReadSync(this.i2cAddress, buffer.length, buffer);
 
         result.actor.x.position = this.readI24(buffer, 0);
@@ -81,10 +104,25 @@ export class Bridge {
         result.joystick.x = this.readI16(buffer, 7);
         result.joystick.y = this.readI16(buffer, 9);
 
+        temp = this.readU8(buffer, 11);
+        result.camera.isFocussing = (temp & 0x01) !== 0;
+        result.camera.isTriggering = (temp & 0x02) !== 0;
+
         return result;
     }
 
-    private writeI24(buffer: Buffer, atIndex: number, value: number) {
+    private write32(buffer: Buffer, atIndex: number, value: number) {
+        buffer[atIndex] = value & 0xff;
+        buffer[atIndex + 1] = value >> 8 & 0xff;
+        buffer[atIndex + 2] = value >> 16 & 0xff;
+        buffer[atIndex + 3] = value >> 24 & 0xff;
+    }
+
+    private readU32(buffer: Buffer, atIndex: number) {
+        return buffer[atIndex + 3] << 24 | buffer[atIndex + 2] << 16 | buffer[atIndex + 1] << 8 | buffer[atIndex];
+    }
+
+    private write24(buffer: Buffer, atIndex: number, value: number) {
         buffer[atIndex] = value & 0xff;
         buffer[atIndex + 1] = value >> 8 & 0xff;
         buffer[atIndex + 2] = value >> 16 & 0xff;
@@ -116,7 +154,7 @@ export class Bridge {
         }
     }
 
-    private writeU8(buffer: Buffer, atIndex: number, value: number) {
+    private write8(buffer: Buffer, atIndex: number, value: number) {
         return buffer[atIndex] = value & 0xff;
     }
 
